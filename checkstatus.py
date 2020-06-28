98106fbc505ab083d64251af9dfe4c94e722c9c5
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-29 00:39:37 +0530

    Update checkstatus.py

--- checkstatus.py
+++ checkstatus.py
@@ -1,5 +1,5 @@
 import os
-if(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 172.17.0.2/index.php)") == "200"):
+if(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 192.168.99.106:3333/index.html)") == "200"):
     pass
 else:
     with open("/status","w") as a:
