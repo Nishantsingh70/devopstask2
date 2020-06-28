eba5c50040a38cb9c8332f3e2494cf93d3b11772
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-29 01:10:33 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -2,6 +2,6 @@ import os
 for root,dirs,files in os.walk('/root/task2'):
     print(files)
     for file in files:
-        if file.endswith('.html'):
+        if file.endswith('.php'):
            os.system("docker container run -dit -p 3333:80 --name phpcontainer -v /root/task2/:/usr/local/apache2/htdocs  phpweb:v2")
            
